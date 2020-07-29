efb5e2e5f99c9309c24e50a27cb194eae185174b
zenthwolf <zenthrelin@gmail.com>
2020-07-29 01:59:33 -0400

    Assertions made ifs for PutPixel - Allows for robust testing with drawrects easily, but maybe lazy?

--- BreezeGameEngine/Graphics.cpp
+++ BreezeGameEngine/Graphics.cpp
@@ -298,11 +298,14 @@ void Graphics::EndFrame()
 
 void Graphics::PutPixel(int x, int y, Color c)
 {
-	assert(x >= 0);
-	assert(x < int(Graphics::ScreenWidth));
-	assert(y >= 0);
-	assert(y < int(Graphics::ScreenHeight));
-	pSysBuffer[Graphics::ScreenWidth * y + x] = c;
+//	assert(x >= 0);
+//	assert(x < int(Graphics::ScreenWidth));
+//	assert(y >= 0);
+//	assert(y < int(Graphics::ScreenHeight));
+	if (x > 0 && x < ScreenWidth && y>0 && y < ScreenHeight)
+	{
+		pSysBuffer[Graphics::ScreenWidth * y + x] = c;
+	}
 }
 
 Color Graphics::GetPixel(int x, int y)
