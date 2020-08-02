1c6a3d40793187f3d9887263a8c68f10d8e9c36a
zenthwolf <zenthrelin@gmail.com>
2020-08-02 02:48:57 -0400

    Sketch of what a "Room" class might look like

---
+++ BreezeGameEngine/Room.cpp
@@ -0,0 +1,98 @@
+#include "Room.h"
+
+void Room::Update(float dt)
+{
+	// Inform dynamic objects that it is time to update!
+	Ava.Update(dt);
+	for (int i = 0; i < Enemy.size(); i++)
+	{
+		Enemy[i].Update(dt);
+	}
+
+	// Should Attacks be updated? How to reconcile joined vs disjoint?
+	// Joined hitbox:
+		// WHat if their "update" just calls on their source to "fix" them?
+	// Disjoint hitbox:
+		// Can be self contained entities, with their own "update" function.
+	
+
+	//Do the collisiony type stuff
+	EnemyCollision(); // Interesting question- which ought be first? Minor detail, however.
+	HitDetection();
+	CheckObstacles(); // end with all characters in good locations
+}
+
+void Room::CheckObstacles()
+{
+	for (int i = 0; i < Obstacle.size(); i++)
+	{
+		//STATIC COLLISION
+
+		Rect<float> staticBox = Obstacle[i].GetCollBox();
+
+		//Check Ava/Obstacle Collision
+		if ( IsColliding( Ava.GetCollBox(), staticBox) )
+		{
+			Ava.PushBox( staticBox ); // A function which replaces Ava to end collision
+		}
+
+		//Check Enemy/Obstacle Collision
+		for (int j = 0; j < Enemy.size(); j++)
+		{
+			Rect<float> enemyBox = Enemy[j].GetCollBox();
+			if (IsColliding( enemyBox, staticBox ))
+			{
+				Enemy.PushBox( staticBox ); // A function which replaces Enemy to end collision- may also alter behavior
+			}
+		}
+	}
+}
+
+void Room::EnemyCollision()
+{
+	//Check Ava/Enemy Collision
+	Rect<float> avaBox = Ava.GetCollBox();
+
+	for (int i = 0; i < Enemy.size(); i++)
+	{
+		Rect<float> enemyBox = Enemy[i].GetCollBox();
+		if (IsColliding(avaBox, enemyBox ))
+		{
+			// Ava has a response- gets hit, flashes, is pushed away, etc.
+			// Enemy may have a response- animation/sound effect, health gain, tactics change
+			// Details of hit may be enemy dependent. The ENEMY should handle the process since it is the most unpredictable piece!
+
+			Enemy[i].Contact(Ava);
+			break; // Only one may hit
+
+			//
+			//	Note: The quality of this hit detection scheme will be interesting with multiple enemies. It may make for eratic recoiling by Ava
+			//	and become unintentionally frustrating. Two possible tunes: Easy- the *closest* contact matters, not the arbitrary first; or Hard-
+			//	an entirely new scheme of hit detection which scans the whole screen more objectively to find a "reasonable" direction
+			//
+		}
+	}
+}
+
+void Room::HitDetection()
+{
+	//
+		// What even is an "Attack"?
+		// Some attacks are affixed to their source (Ava's swing) and will recoil on their source
+		// Some attacks are disjoint from their source (a fireball or energy beam, which cannot affect their source
+		// Some attacks should play sides (Enemies don't hurt each other; *most* of Ava's attacks should not affect her
+		// Attacks may need a reference to their particular source (for recoil) or "side" at least
+	//
+}
+
+bool Room::IsColliding(const Rect<float> box1, const Rect<float> box2) const
+{
+	if ("box 1 collides with box 2")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
---
+++ BreezeGameEngine/Room.h
@@ -0,0 +1,25 @@
+#pragma once
+//Playground to identify needed interactions
+//Eventual baseline for "Room" class, but don't take this shit seriously.
+
+#include "Character.h"
+#include "Enemy.h"
+#include "Rect.h"
+#include <vector>
+
+
+class Room
+{
+private:
+	void Update(float dt);
+	void CheckObstacles(); //Check entities are not colliding with obstacles
+	void EnemyCollision(); //Check if Ava is in contact with an enemy
+	void HitDetection(); //Detect Collision of attacks
+	bool IsColliding(const Rect<float> box1, const Rect<float> box2 ) const; //Do these objects collide?
+
+private:
+	Character& Ava; //Does Ava properly belong to the "Room" she is in? Or do rooms just "know" about her?
+	std::vector<Enemy> Enemy; // Should have a list of enemies- ALL ENEMIES ARE REQUIRED TO DO ANYTHING STATED HERE, SO BASIS FOR SUBCLASS!
+	std::vector<Rect<float>> Obstacle; // List of obstacles- which will eventually be a class like "ground" once was.
+	std::vector<Rect<float>> Attack; // List of attacks? Are these to be handled by their sources? Is that even possible?
+};
